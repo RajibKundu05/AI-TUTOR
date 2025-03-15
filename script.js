f88a620b524623a943525910fa099a457d0fdcc1
RajibKundu05 <kundurajib05@gmail.com>
2025-03-15 12:18:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn1 = document.getElementById("watch-video");
+const btn2 = document.getElementById("ask-question");
+const btn3 = document.getElementById('learn-more');
+
+btn1.addEventListener("click", redirectToAskQtn);
+
+function redirectToAskQtn(){
+    alert("Hello! This is an alert.");
+}
